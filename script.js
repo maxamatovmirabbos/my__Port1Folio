8fd9fdc42a4a9c8184465f7589dba58ec1c41d06
unknown <maxamatovmirabbos>
2024-12-25 09:02:31 +0500

    accordion 2-bosqich start

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ accordions.forEach(item => {
     item.classList.toggle('active');
   });
 });
+
+ // Accordion funksiyasi
+ const accordionBlocks = document.querySelectorAll('.accordion-block');
+
+ accordionBlocks.forEach(block => {
+   const title = block.querySelector('.accordion-title');
+
+   title.addEventListener('click', () => {
+     // Boshqalarni yopish
+     accordionBlocks.forEach(item => {
+       if (item !== block) item.classList.remove('is-active');
+     });
+
+     // Joriy accordionni ochish/yopish
+     block.classList.toggle('is-active');
+   });
+ });
